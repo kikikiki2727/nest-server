85f0d8a7e4e55dee3c807bf451c6c5de1a23c8e2
kikikiki2727 <kimori27271212@gmail.com>
2022-06-02 19:43:43 +0900

    feat:　環境変数の設定

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,14 +2,27 @@ import {
   Controller,
   Get,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
 
 @Controller()
 export class AppController {
   constructor(
-  ) {}
+    private configService: ConfigService,
+  ) {
+  }
 
   @Get('sample')
   sample(): string {
     return 'sample';
+  }
+
+  @Get('valiables')
+  valiables(): object {
+    const vonageApiKey: string = this.configService.get<string>('VONAGE_API_KEY')
+    const vonageSecret: string = this.configService.get<string>('VONAGE_SECRET')
+    return {
+      vonageApiKey,
+      vonageSecret
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PostModule],
+  imports: [
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
