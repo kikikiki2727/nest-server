c0a03cf65eaaab8d6008588f905ccd0c523cc047
kikikiki2727 <kimori27271212@gmail.com>
2022-06-10 19:07:03 +0900

    fix: repository層に処理切り分け

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+// import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
 import { UserController } from './user.controller';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [UserController,],
-  providers: [UserService, PrismaService]
+  providers: [UserRepository, PrismaService]
 })
 export class UserModule {}
